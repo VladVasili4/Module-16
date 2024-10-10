4d1d326a539613eaa56c072c0d223092c252098e
VladVasili4 <dipol777@ya.ru>
2024-10-10 07:29:46 +0300

    Add files via upload

--- homework_16_3.py
+++ homework_16_3.py
@@ -10,25 +10,23 @@ async def get_users() -> dict:
     return users
 
 @app.post('/user/{username}/{age}')
-async def create_user(username: str, age: str) -> str:
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
+                        age: Annotated[int, Path(ge=18, le=120, description="Enter age", examples='24')]) -> str:
     user_id = str(int(max(users, key=int))+1)
-    users[user_id] = 'Имя: username, возраст: age'
+    users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'User {user_id} is registered'
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_message(user_id: str, username: str, age: str) -> str:
-    users[user_id] = 'Имя: username, возраст: age'
-    return f"The user {user_id} has been registered"
+async def update_message(user_id: Annotated[str, Path(description='Enter user_id', example='2')],
+                         username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
+                        age: Annotated[int, Path(ge=18, le=120, description="Enter age", examples='24')]) -> str:
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f"The user {user_id} has been updated"
 
 @app.delete('/user/{user_id}')
-async def delete_message(user_id: str) -> str:
+async def delete_message(user_id: Annotated[str, Path(description='Enter user_id for delete. Warning! All data will be destroyed')]) -> str:
     users.pop(user_id)
-    return f'User {user_id} has been deleted"'
-
-
-
-
-
+    return f'User {user_id} has been deleted'
 
 
 # python -m uvicorn main:app
