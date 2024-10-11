f40c391bbbeffbfc468b37a049a954b98debd282
VladVasili4 <dipol777@ya.ru>
2024-10-11 07:38:31 +0300

    Update homework_16_3.py

--- homework_16_3.py
+++ homework_16_3.py
@@ -17,15 +17,14 @@ async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,
     return f'User {user_id} is registered'
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_message(user_id: Annotated[str, Path(description='Enter user_id', example='2')],
+async def update_user(user_id: Annotated[str, Path(description='Enter user_id', example='2')],
                          username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
                         age: Annotated[int, Path(ge=18, le=120, description="Enter age", examples='24')]) -> str:
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f"The user {user_id} has been updated"
 
 @app.delete('/user/{user_id}')
-async def delete_message(user_id: Annotated[str, Path(description='Enter user_id for delete. Warning! All data will be destroyed')]) -> str:
-    users.pop(user_id)
+async def delete_user
     return f'User {user_id} has been deleted'
 
 
