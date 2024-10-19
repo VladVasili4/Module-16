9d6fa5f4aee6b4ab41b5bc26a37386e3ea9b56e6
VladVasili4 <dipol777@ya.ru>
2024-10-19 15:35:25 +0300

    Update homework_16_4.py

--- homework_16_4.py
+++ homework_16_4.py
@@ -15,27 +15,27 @@ def get_users() -> List[User]:
     return users
 
 @app.post('/user/{username}/{age}')
-def create_user(username: str, age: int, user: User) -> User:
-    user.id = len(users)+1
-    user.username = username
-    user.age = age
+def create_user(username: str, age: int):
+    user_id = len(users) + 1
+    user = User(id=user_id, username=username, age=age)
     users.append(user)
     return user
 
 
 @app.put('/user/{id}/{username}/{age}')
-def update_user(id: int, username: str, age: int, user: User = Body()) -> User:
+async def update_user(id: int, username: str, age: int):
     try:
         edit_user = users[id-1]
         edit_user.username = username
         edit_user.age = age
-        return user
+        return edit_user
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
 
 @app.delete('/user/{id}')
-def delete_user(id: int, user: User ) -> User:
+def delete_user(id: int): 
     try:
+        user = User()
         users.pop(id-1)
         return user
     except IndexError:
